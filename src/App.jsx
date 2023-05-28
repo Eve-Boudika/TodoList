147043550d1fab7a5781ae273163649ce3df0185
Evelyn <evelabohemia@gmail.com>
2023-05-28 13:38:57 -0300

    Creación de la App, subida primigenia, instalación yarn, material, limpieza del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from '@mui/material'
+
+function App() {
+
+  return (
+    <>
+      <Button variant="contained">Contained</Button>
+    </>
+  )
+}
+
+export default App
