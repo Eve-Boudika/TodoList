344dc6fe3ed20f75cd1cc21655cfd511ed0a7d41
Evelyn <evelabohemia@gmail.com>
2023-05-28 14:54:44 -0300

    llamada al json para mostrar las tareas

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,33 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import Tareas from '../public/Database.json'
 
 export const Task = () => {
-        const [tasks, setTasks] = useState([]);
+  
       
-        useEffect(() => {
-          const fetchTasks = async () => {
-            try {
-
-                        //Consultar a Ganso sobre si la ruta está ok o si falta algo mas en el json//
-
-              const response = await fetch('./Database.json');
-              const data = await response.json();
-              setTasks(data);
-            } catch (error) {
-              console.error('Error al obtener las tareas:', error);
-            }
-          };
       
-          fetchTasks();
-        }, []);
 
   return (
     <div>
     <h1>Tareas:</h1>
-    {tasks.map(task => (
-      <div key={task['nombre de tarea']}>
-        <h2>{task['nombre de tarea']}</h2>
-        <p>Días: {task.dias.join(', ')}</p>
-        <p>Necesita comprar: {task['necesita comprar'] ? 'Sí' : 'No'}</p>
+    {Tareas.map(tarea => (
+      <div key={tarea['nombre de tarea']}>
+        <h2>{tarea['nombre de tarea']}</h2>
+        <p>Días: {tarea.dias.join(', ')}</p>
+        <p>Necesita comprar: {tarea['necesita comprar'] ? 'Sí' : 'No'}</p>
       </div>
     ))}
   </div>
